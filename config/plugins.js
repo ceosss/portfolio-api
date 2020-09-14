976442e5790ba6a1db6eab6796991258376f6092
ceosss <sswarajsamant@gmail.com>
2020-09-14 20:39:22 +0530

    INSTALLED: Cloudinary

---
+++ config/plugins.js
@@ -0,0 +1,10 @@
+module.exports = () => ({
+  upload: {
+    provider: "cloudinary",
+    providerOptions: {
+      cloud_name: `dklqecivk`,
+      api_key: `645254347827965`,
+      api_secret: `cfD9AOEMSFbOy9Ml8P_1x_4h2OA`,
+    },
+  },
+});
